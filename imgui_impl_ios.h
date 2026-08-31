15c343dc6743a7f643c19e098bfb9f67b51c09e0
Trần Quang Hải <tranhuynh9123@gmail.com>
2026-08-31 23:27:04 +0700

    Delete imgui_impl_ios.h

--- imgui_impl_ios.h
+++
@@ -1,8 +0,0 @@
-// imgui_impl_ios.h
-#pragma once
-#include "imgui.h"
-
-bool ImGui_ImplIOS_Init(void* view, void* context);
-void ImGui_ImplIOS_Shutdown();
-void ImGui_ImplIOS_NewFrame();
-void ImGui_ImplIOS_RenderDrawData(ImDrawData* draw_data);
